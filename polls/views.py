002bbfae40b9d305fe8ba77080c0353e3bc554cb
Hadrian Kalski <hka@Hadrians-MacBook-Air.local>
2013-12-07 20:54:37 +0100

    replaced hello world to datetime.now() - in views.py

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def hello(request):
-   return HttpResponse("Hello world!")
+
+   now = datetime.datetime.now()
+   html = "<html><body>It is now %s.</body></html>" % now
+   return HttpResponse(html)
