d577bee5e82bc08cbf7e1cd35ed3ba50e86e38fd
Hadrian Kalski <hka@Hadrians-MacBook-Air.local>
2013-12-07 21:25:52 +0100

    added hours ahead

--- ecofective/urls.py
+++ ecofective/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from polls.views import hello, current_datetime
+from polls.views import hello, current_datetime, hours_ahead
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^$', current_datetime),
 )
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime
 
 # Create your views here.
@@ -12,4 +12,15 @@ def current_datetime(request):
 
    now = datetime.datetime.now()
    html = "<html><body>It is now %s.</body></html>" % now
+   return HttpResponse(html)
+
+def hours_ahead(request, offset):
+
+   try:
+      offset = int(offset)
+   except:
+      raise Http404()
+
+   dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+   html = "<html><body>in %s hour(s), it will be %s. </body></html>" % (offset, dt)
    return HttpResponse(html)
